2ff0d7fa6a6ccf8bfd815947897085f0f53d9e0b
Jonathan Petrinolis <jpetrinolis@gmail.com>
2020-10-28 22:23:25 +1030

    Create app state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import App from './App';
+import rootReducer from './store';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <Provider store={store}> 
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/useApp.js
@@ -0,0 +1,10 @@
+
+const useApp = () => {
+  const counter = 100;
+
+  return {
+    counter
+  }
+}
+
+export default useApp
